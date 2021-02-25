7a72be2218f8a53f447bfa658fe8c4ee47d2ef91
Sandeep <sandeep.recraft@gmail.com>
2021-02-25 11:12:25 +0530

    implement eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,14 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "import/no-extraneous-dependencies": ["error", { "devDependencies": false, "optionalDependencies": false }],
+        "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
+        "no-console": ["error", { "allow": ["log", "error", "warn"] }],
+        "no-multiple-empty-lines": "off",
+        "object-curly-newline": "off",
+        "import/no-cycle": "off",
+        "react/destructuring-assignment": "off",
+        "react/no-access-state-in-setstate": "off",
+        "react/jsx-props-no-spreading": "off"
     }
 };
