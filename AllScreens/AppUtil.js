6a1b4e3d8fa977c7e4d1c6dc40ca5ff4922e3c59
kanika goyal <kanika.recraft@gmail.com>
2021-03-01 18:00:29 +0530

    Made changes in UI of screen5

--- AllScreens/AppUtil.js
+++ AllScreens/AppUtil.js
@@ -7,4 +7,8 @@ export const AppColor = {
     blackGrey:'#696969',
     lineGrey:'#393F45',
     blackBlue:'#202D7E',
+    lightPurple:'#D6DBFD',
+    borderBlue:'#92A1FC',
+    backBlue:'#5E73FF',
+    cream:'#F5F5F5',
 }
