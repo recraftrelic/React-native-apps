2c9d274e6e4dd49eae87b328da6c828dd21b6118
Recraft-5 <recraft-5@Recraft-5s-MacBook-Air.local>
2021-03-02 17:41:19 +0530

    LoginScreen5

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { Route, Switch, NativeRouter } from "react-router-native";
-// import LoginScreen2 from './components/LoginScreen2';
-import LoginScreen3 from './components/LoginScreen3';
-// import LoginScreen1 from './components/login-pages/LoginScreen1';
-// import LoginScreen2 from './components/LoginScreen2';
-// import LoginScreen1 from './components/login-pages/LoginScreen1';
+import { NativeRouter, Route, Router, Switch } from 'react-router-native';
+import MainScreen from './AllScreens/MainScreen/MainScreen';
+import 'react-native-gesture-handler';
+// import { createAppContainer } from 'react-navigation';
+// import { createStackNavigator} from 'react-navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Screen1 from './AllScreens/Screen1';
+import LoginScreen1 from './components/login-pages/LoginScreen1'
+import LoginScreen10 from './components/LoginScreen10';
+import LoginScreen from './components/LoginScreen';
+import LoginScreen8 from './components/LoginScreen8';
+import LoginScreen4 from './components/LoginScreen4';
+//import Screen1 from "./AllScreens/Screen1"
+const Stack = createStackNavigator();
+const App  = () => {
+  
+  return (
+    // <LoginScreen/>
+    <LoginScreen1/>
+    // <LoginScreen10/>
+    // <LoginScreen8/>
+    // <LoginScreen4/>
+    // <NavigationContainer>
+    //  <Stack.Navigator>
+    //     <Stack.Screen
+    //        name="page1"
+    //       component={MainScreen}
+    //       options={{ title: 'Press to go to the Screen' }}
+    //     />
+    //     <Stack.Screen name="screen1" component ={LoginScreen2}/>
+    //     </Stack.Navigator>
+    // </NavigationContainer>
+    
+  );
 
-const App = () => {
-    return (
-      <NativeRouter>
-      <Switch>
-      <Route exact path="/" component={LoginScreen3} />
-      </Switch>
-      </NativeRouter>
-    );
 };
+
 
 export default App;
