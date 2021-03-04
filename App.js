0703c193e173c17df2638cc6cf86c22ecc172400
sandeep kumar <sandeep.recraft@gmail.com>
2021-03-04 13:13:47 +0530

    done

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Screen2 from './AllScreens/Screen2/Screen2';
 import Screen5 from './AllScreens/Screen5/Screen5';
 import Screen6  from './AllScreens/Screen6/Screen6';
 import Screen9 from './AllScreens/Screen9/Screen9'
+import LoginScreen10 from './components/LoginScreen10';
 
 
 const App = () => {
@@ -13,7 +14,7 @@ const App = () => {
       <Switch>
         {/* <Route exact path="/" component={LoginScreen2} /> */}
         {/* <Route exact path="/base/" component={BaseNext} />  */}
-        <Route exact path="/" component={Screen2}></Route>
+        <Route exact path="/" component={LoginScreen10}></Route>
         {/* <Route exact path="/" component={Screen1}></Route> */}
         {/* <Route exact path="/" component={Screen5}></Route> */}
         {/* <Route exact path="/" component={Screen6}></Route> */}
