ac9dc8512fa6a881272a032b0c1b9931aaf2dde0
sandeep kumar <sandeep.recraft@gmail.com>
2021-03-04 12:15:37 +0530

    solve conflict

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import Screen2 from './AllScreens/Screen2/Screen2';
 import Screen5 from './AllScreens/Screen5/Screen5';
 import Screen6  from './AllScreens/Screen6/Screen6';
 import Screen9 from './AllScreens/Screen9/Screen9'
-import LoginScreen4 from './components/LoginScreen7';
-
 
 
 const App = () => {
@@ -15,7 +13,7 @@ const App = () => {
       <Switch>
         {/* <Route exact path="/" component={LoginScreen2} /> */}
         {/* <Route exact path="/base/" component={BaseNext} />  */}
-        <Route exact path="/" component={LoginScreen4}></Route>
+        <Route exact path="/" component={Screen2}></Route>
         {/* <Route exact path="/" component={Screen1}></Route> */}
         {/* <Route exact path="/" component={Screen5}></Route> */}
         {/* <Route exact path="/" component={Screen6}></Route> */}
